61b4853e0e616491a52a70f4e8bcf37dcb01071a
stock2018-ai <liang.wujian@googlemail.com>
2026-07-15 03:13:25 +0200

    Update stock_monitor.py

--- stock_monitor.py
+++ stock_monitor.py
@@ -1,1 +1,15 @@
-print("只能做T助手启动")
+import datetime
+
+print("智能做T助手启动")
+print("运行时间：", datetime.datetime.now())
+
+stock = "中国卫星 600118"
+
+print("正在监控：", stock)
+
+print("分析指标：")
+print("- 均线")
+print("- RSI")
+print("- MACD")
+print("- 成交量")
+print("- 0.618回调")
